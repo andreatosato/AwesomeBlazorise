db530e336c231c8a21871c34948e31bfd1ef7ad0
Andrea Tosato <andrea.tosato@hotmail.it>
2021-03-13 10:13:20 +0100

    Fix #2

---
+++ src/AwesomeBlazor/wwwroot/helper.js
@@ -0,0 +1,16 @@
+﻿window.blazorHelpers = {
+    scrollToFragment: (elementId) => {
+        var element = document.getElementById(elementId);
+
+        if (element) {
+            element.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    }
+};
+
+window.blazorCulture = {
+    get: () => window.localStorage['BlazorCulture'],
+    set: (value) => window.localStorage['BlazorCulture'] = value
+};
